a6afedad28b1d7c9a22dca710a5d21c6d7098f27
usman18 <secretdeveloper18@gmail.com>
2019-07-10 12:46:17 +0530

    -> Both themes setup

--- app/src/main/java/com/uk/darkthemeexample/MainActivity.java
+++ app/src/main/java/com/uk/darkthemeexample/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setTheme(R.style.LightTheme);
 		setContentView(R.layout.activity_main);
 	}
 }
