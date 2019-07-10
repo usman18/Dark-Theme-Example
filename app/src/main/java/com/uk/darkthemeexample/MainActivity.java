94693d378f43851fd896d7fb40d0a0a2a263f709
usman18 <secretdeveloper18@gmail.com>
2019-07-10 13:54:24 +0530

    -> Completed.

--- app/src/main/java/com/uk/darkthemeexample/MainActivity.java
+++ app/src/main/java/com/uk/darkthemeexample/MainActivity.java
@@ -1,14 +1,41 @@
 package com.uk.darkthemeexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setTheme(R.style.LightTheme);
+		
+		if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+			setTheme(R.style.DarkTheme);
+		} else {
+			setTheme(R.style.LightTheme);
+		}
+		
 		setContentView(R.layout.activity_main);
+		
+		TextView tvChangeTheme = findViewById(R.id.tvChangeTheme);
+		tvChangeTheme.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+				} else {
+					AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+				}
+				
+				finish();
+				startActivity(new Intent(MainActivity.this, MainActivity.this.getClass()));
+			}
+		});
+		
 	}
 }
